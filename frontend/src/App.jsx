3256bb337ab11f44271dabeee8880c8639c19c3b
Shaik Yasmin-786 <shaiky1713@gmail.com>
2024-09-19 11:44:26 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,44 @@
 
 import React from 'react';
-import './App.css'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/login";
+import SignUp from "./components/login.js";
+import {RequireToken} from './components/Auth.js'
+ 
+import Dashboard from "./components/dashboard";
+import Home from "./components/home";
+import Employee from "./components/employee";
+import Profile from "./components/profile";
+import AddEmployee from "./components/addemployee";
+import EditEmployee from './components/editemployee'
+ 
 function App() {
   return (
-    <>
-      This is a project.
-    </>
-  )
+    <div className="app">
+        <BrowserRouter>
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<SignUp />} />
+               
+              <Route path='/' element={
+                  <RequireToken>
+                    <Dashboard />
+                  </RequireToken>
+                  }>
+                  <Route path='' element={<Home/>}></Route>
+                  <Route path='/employee' element={<Employee />}></Route>
+                  <Route path='/profile' element={<Profile />}></Route>
+                  <Route path='/create' element={<AddEmployee />}></Route>
+                  <Route path='/employeeedit/:id' element={<EditEmployee />}></Route>
+              </Route>
+            </Routes>
+        </BrowserRouter>
+    </div>
+  );
 }
+   
+export default App;
+  
 
-export default App
+
+
