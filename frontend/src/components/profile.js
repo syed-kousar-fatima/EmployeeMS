bf2e024824d7a566d6046ca2ac4ed8d678f92c83
Shaik Yasmin-786 <shaiky1713@gmail.com>
2024-09-19 11:59:10 +0530

    adding files

---
+++ frontend/src/components/profile.js
@@ -0,0 +1,15 @@
+//C:\react-js\my-app\src\components\profile.js
+import React, { } from "react";
+
+export default function Profile() {
+    return (
+        <div className="container" style={{ paddingTop: 60 }}>
+            <div className="row">
+                <div style={{ minHeight: 800, marginTop: 20 }}>
+                    <h1>Profile Page</h1>
+                    <p>Hi, this is your profile</p>
+                </div>
+            </div>
+        </div>
+    )
+}
