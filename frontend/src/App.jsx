0e2cb568714ada4e70f5e18079a1191fdbfbb624
Shaik Yasmin-786 <shaiky1713@gmail.com>
2024-09-20 16:35:14 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { RequireToken } from './components/Auth.jsx';
 import Login from './components/login.jsx';
 import Signup from './components/signup.jsx';
 import Dashboard from './components/dashboard.jsx';
@@ -9,16 +10,8 @@ import Employee from './components/employee.jsx';
 import AddEmployee from './components/addemployee.jsx';
 import EditEmployee from './components/editemployee.jsx';
 import Indexpage from './components/Indexpage/index.jsx';
-// import Login from "./components/login";
-// import SignUp from "./components/login.js";
-// import {RequireToken} from './components/Auth.js'
- 
-// import Dashboard from "./components/dashboard";
-// import Home from "./components/home";
-// import Employee from "./components/employee";
-// import Profile from "./components/profile";
-// import AddEmployee from "./components/addemployee";
-// import EditEmployee from './components/editemployee.js';
+import Profile from './components/profile.jsx';
+
 
  
 function App() {
@@ -28,9 +21,9 @@ function App() {
             <Routes>
               <Route path='/index' element={<Indexpage/>}/>
               <Route path="/login" element={<Login/>} />
-              <Route path="/signup" element={<Signup/>} />
-               
-              <Route path='/' element={
+             <Route path="/signup" element={<Signup/>} />
+             
+             <Route path='/' element={
                   <RequireToken>
                     <Dashboard/>
                   </RequireToken>
