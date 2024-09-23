993acb395de7d55b21d62603cee301e8bf7583ba
syed-kousar-fatima <syedkousar99@gmail.com>
2024-09-23 21:08:01 +0530

    modify

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav className=" navbar navbar-expand-lg navbar-light bg-light">
+        <nav className=" navbar navbar-expand-lg  navbar-light bg-body-light">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/">Employee Management System</Link>
+                <Link className="navbar-brand" to="/"><h2>Employee Management System</h2></Link>
                 <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
+                    <ul className="navbar-nav ">
                         <li className="nav-item">
                             <Link className="nav-link" to="/employee-dashboard">Employee Dashboard</Link>
                         </li>
