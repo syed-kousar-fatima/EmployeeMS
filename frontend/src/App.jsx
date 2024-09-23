0b926f17945178266633e6df7e59bbf12f6e1136
syed-kousar-fatima <syedkousar99@gmail.com>
2024-09-23 13:13:36 +0530

    added:

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <div className="app">
         <BrowserRouter>
             <Routes>
-              
+              <Route/>
             </Routes>
         </BrowserRouter>
     </div>
